7f69f4b4bd901a1f48b8a2671cd1013ddd81b25d
n <n>
2024-09-24 19:21:30 +0700

    update splash screen for apk

--- App.js
+++ App.js
@@ -78,8 +78,8 @@ function ReportMenu({ navigation,route}) {
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-  let [fontsLoaded] = useFonts({
+
+  let [fontsLoaded, fontError] = useFonts({
     'Roboto-Italic': require('./assets/fonts/Roboto-Italic.ttf'),
     'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
     'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
@@ -87,38 +87,22 @@ function App() {
   });
 
   useEffect(() => {
-    async function prepare() {
-      try {
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync(fontsLoaded);
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Please remove this if you copy and paste the code!
-        await new Promise(resolve => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
     }
-
-    prepare();
-  }, []);
+  }, [fontsLoaded, fontError]);
 
   const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
+    if (fontsLoaded || fontError) {
       await SplashScreen.hideAsync();
     }
-  }, [appIsReady]);
+  }, [fontsLoaded, fontError]);
 
-  if (!appIsReady) {
+  if (!fontsLoaded && !fontError) {
     return null;
   }
+
+  
    
   return (
     <Provider store={store}>
--- source/constants/theme.js
+++ source/constants/theme.js
@@ -47,7 +47,7 @@ export const SIZES = {
 };
 
 export const FONTS = {
-    largeTitle: { fontFamily: "Roboto-regular", fontSize: SIZES.largeTitle, lineHeight: 55 },
+    largeTitle: { fontFamily: "Roboto-Regular", fontSize: SIZES.largeTitle, lineHeight: 55 },
     h1: { fontFamily: "Roboto-Black", fontSize: SIZES.h1, lineHeight: 36 },
     h2: { fontFamily: "Roboto-Bold", fontSize: SIZES.h2, lineHeight: 30 },
     h3: { fontFamily: "Roboto-Bold", fontSize: SIZES.h3, lineHeight: 22 },
